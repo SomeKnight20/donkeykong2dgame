da625a686e3384570c54c86645b6cd71de49346f
SomeKnight20 <teemu.huttunen20@gmail.com>
2023-03-31 17:01:32 +0300

    tutorial stuff

--- tutorial/Donkey Kong/Assets/Scripts/PlayerController.cs
+++ tutorial/Donkey Kong/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     private Rigidbody2D player;
 
     float horizontalInput;
@@ -17,24 +18,68 @@ public class PlayerController : MonoBehaviour
     private bool canClimb = false;
     private bool isClimbing = false;
 
+    private Transform ladder;
+    private float playerHeight;
+
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        playerHeight = GetComponent<SpriteRenderer>().size.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (grounded){
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+        }
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyDown("space") && grounded){
+        if (Input.GetKeyDown("space") && grounded && !isClimbing){
             player.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
+        movement.y = 0;
+        if (canClimb){
+            if (verticalInput != 0 && grounded && horizontalInput == 0){
+                isClimbing = true;
+            }
+        }else{
+            isClimbing = false;
+        }
+        if(horizontalInput != 0){
+            isClimbing = false;
+        }
+
+        if (isClimbing){
+            if (player.position.y <= ladder.transform.GetChild(0).transform.position.y+playerHeight/2
+                && player.position.y >= ladder.transform.GetChild(1).transform.position.y-0.1f){
+                player.velocity = Vector2.zero;
+                player.isKinematic = true;
+                movement.y = verticalInput * moveSpeed;
+                player.position = new Vector2(ladder.transform.position.x, player.position.y);
+                transform.localScale = new Vector3(Mathf.Round(Mathf.Abs(player.position.y) % 1) * 2 - 1, 1, 1);
+            }else{
+                isClimbing = false;
+            }
+        }else{
+            player.isKinematic = false;
+        }
+
+        if (horizontalInput > 0){
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (horizontalInput < 0){
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
         movement.x = horizontalInput * moveSpeed;
+
+        animator.SetFloat("speed", Mathf.Abs(horizontalInput));
+        animator.SetBool("climbing", isClimbing);
+        animator.SetBool("grounded", grounded);
     }
 
     void FixedUpdate()
@@ -50,6 +95,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ladder"))
         {
             canClimb = true;
+            ladder = collision.transform;
         }
     }
     void OnTriggerExit2D(Collider2D collision)
